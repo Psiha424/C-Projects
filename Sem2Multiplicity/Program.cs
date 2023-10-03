c2ca8bad31f033ebd9d40cc99519630d4386a75e
Const <psiha424@gmail.com>
2023-10-03 12:38:16 +0300

    2 задание для самопроверки, не особо успеваю

---
+++ Sem2Multiplicity/Program.cs
@@ -0,0 +1,19 @@
+﻿void CheckMultiplicity(int num1, int num2)
+{
+    if(num1 % num2 == 0)
+    {
+        System.Console.WriteLine($"{num1}, {num2} -> кратно");
+    }
+    else
+    {
+        System.Console.WriteLine($"{num1}, {num2} -> не кратно, остаток {num1 % num2}");
+    }
+}
+
+Console.Write("Введите первое число:");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число:");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+CheckMultiplicity(n1, n2);
