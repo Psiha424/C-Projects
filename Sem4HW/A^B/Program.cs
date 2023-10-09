ffc8367f054eabf99c7b2a64cd38144ab8505ce1
Const <psiha424@gmail.com>
2023-10-09 20:13:21 +0300

    задание для самопроверки, 2 из 3, но лениво

---
+++ Sem4HW/A^B/Program.cs
@@ -0,0 +1,33 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readImput = System.Console.ReadLine();
+    int result = int.Parse(readImput);
+    return result;
+}
+int Power(int powerBase, int exponent)
+{
+    int power = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        power *= powerBase;
+    }
+    return power;
+};
+
+bool ValidateExponent(int exponent)
+{
+    if (exponent < 0)
+    {
+        System.Console.WriteLine("Показатель меньше 0");
+        return false;
+    }
+    return true;
+};
+
+int powerBase = Prompt("Введите основание: ");
+int exponent = Prompt("Введите степень: ");
+if (ValidateExponent(exponent))
+{
+    System.Console.Write($"Число {powerBase} в степени {exponent} равно {Power(powerBase, exponent)}");
+}
---
+++ Sem4HW/SumCifr/Program.cs
@@ -0,0 +1,21 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readImput = System.Console.ReadLine();
+    int result = int.Parse(readImput);
+    return result;
+}
+
+int SumAllDigit(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+int number = Prompt("Введите число: ");
+System.Console.Write($"Сумма цифр равна {number} = {SumAllDigit(number)}");
