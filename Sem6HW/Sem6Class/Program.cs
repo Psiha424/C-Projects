baecbc5ff48692ed329aec294aaa48c68d3a5429
Const <psiha424@gmail.com>
2023-10-25 17:39:18 +0300

    6 семинар закончаен

---
+++ Sem6HW/Sem6Class/Program.cs
@@ -0,0 +1,43 @@
+﻿int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue +1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int[] ReverseArray(int[] array)
+{
+    int temp;
+    for (int i = 0; i < array.Length/2; i++)
+    {
+        temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+    return array;
+}
+System.Console.Write("Input size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Input min value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Input max value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(size, min, max);
+PrintArray(myArray);
+myArray = ReverseArray(myArray);
+PrintArray(myArray);
---
+++ Sem6HW/Sem6Class2/Program.cs
@@ -0,0 +1,14 @@
+﻿string DecimalToBinary(int num)
+{
+    string result = string.Empty;
+    while(num > 0)
+    {
+        result = num % 2 + result;
+        num /=2;
+    }
+    return result;
+}
+
+System.Console.WriteLine("Imput number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(DecimalToBinary(num));
