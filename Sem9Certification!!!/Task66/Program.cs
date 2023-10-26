a537601d5373f51f03d309d342f48ba2cff28410
Const <psiha424@gmail.com>
2023-10-26 20:05:53 +0300

    завершение дз к 9 семинару

---
+++ Sem9Certification!!!/Task66/Program.cs
@@ -0,0 +1,25 @@
+﻿int SumMbetweenN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMbetweenN(m, n);
+        return res;
+    }
+}
+
+Console.Write("Input number M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumMToN(m, n);
+
+void SumMToN(int m, int n)
+{
+    Console.Write(SumMbetweenN(m - 1, n));
+}
