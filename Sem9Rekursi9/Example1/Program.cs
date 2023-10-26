e06e424afc8857d2f4e92b0b7f51b3d08ae79335
Const <psiha424@gmail.com>
2023-10-26 19:05:39 +0300

    Комитим примеры рекурсии с крайнего 9 семинара

---
+++ Sem9Rekursi9/Example1/Program.cs
@@ -0,0 +1,10 @@
+﻿void ShowNums(int n)
+{
+    if (n > 0)
+    {
+        ShowNums(n - 1);
+        System.Console.Write(n + " ");
+
+    }
+}
+ShowNums(10);
---
+++ Sem9Rekursi9/Example2/Program.cs
@@ -0,0 +1,10 @@
+﻿int FindSum(int num)
+{
+    if(num > 0)
+    {
+        return FindSum(num / 10) + num % 10;
+    }
+    else return num;
+}
+
+System.Console.WriteLine(FindSum(567));
