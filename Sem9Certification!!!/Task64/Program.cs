e8a0bbd9d053b20a6ed6c94bb7935648ce5ca391
Const <psiha424@gmail.com>
2023-10-26 19:15:38 +0300

    64 task is ready

---
+++ Sem9Certification!!!/Task64/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Input N nunmber: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+OutputNumbers(number, count);
+
+void OutputNumbers(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        OutputNumbers(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
